4dc8777c070d5a4bf11cac81649acc08e117c29a
원정호 <jhwon_00@naver.com>
2018-05-13 05:11:35 +0900

    레이아웃 수정 중

---
+++ Vocablist/inspect_word.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	var vocab_sound_icon = document.getElementById('vocab_sound_icon');
+	
+	vocab_sound_icon.onclick = function () {
+		alert("This is not implemented in this prototype!");
+	};
+	
+});
--- Vocablist/vocablist.js
+++ Vocablist/vocablist.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-	
-}
+
+	function render_box_contents() {
+
+	}
+
+	var context_container_box = document.getElementById('context_container_box');
+
+	render_box_contents();
+});
