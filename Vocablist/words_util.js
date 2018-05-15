0a76a2242118e58f4a1536f4a5a4de422ee14a58
원정호 <jhwon_00@naver.com>
2018-05-16 02:57:13 +0900

    단어가 존재하는지 여부의 함수 만듦

--- Vocablist/words_util.js
+++ Vocablist/words_util.js
@@ -2,12 +2,26 @@
 	var contextsRef = firebase.database().ref('contexts');
 	var vocabsRef = firebase.database().ref('vocabs');	
 
-	var vocabFromID = function (id, callback) {
+	function vocabFromID (id, callback) {
 		vocabsRef.orderByChild('id').equalTo(id).once('value').then(function (snapshot) {
+			//console.log(snapshot.val());
 			callback(Object.values(snapshot.val())[0]);
 		});
-	};
 
+		// Example
+		// vocabFromID(31, vocab => alert(vocab.korean)); // '사랑하다'가 출력됨
+	}
+
+	function isVocabInList (context_chapter, vocab_id, callback) {
+		contextsRef.orderByChild('chapter').equalTo(context_chapter).once('value').then(function (snapshot) {
+			callback(Object.values(Object.values(snapshot.val())[0].vocabs).indexOf(vocab_id) > -1);
+		});
+
+		// Example
+		// isVocabInList(1, 31, includes => alert(includes)); // True 또는 False가 출력됨
+	}
+
+	//isVocabInList(1, 31, includes => console.log(includes));
 	/*
 	function getContextFromVocab(vocab_id) {
 		var ret = [];
