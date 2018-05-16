6c35c97bde446af8d99b1a44ba887805ef9ba08f
LieAnn <quinn_akrso@kaist.ac.kr>
2018-05-17 02:32:41 +0900

    popup
    프로필 누르면 context설정창이 뜨도록 만들었습니다

---
+++ popup.js
@@ -0,0 +1,27 @@
+
+$(function(){
+  $('#class').click(function(){
+                    $('#class').hide();
+                      });
+  });
+
+
+$(function(){
+  $('#sick').click(function(){
+                    $('#sick').hide();
+                    });
+  });
+
+
+$(function(){
+  $('#hobby').click(function(){
+                    $('#hobby').hide();
+                    });
+  });
+
+
+$(function(){
+  $('#drink').click(function(){
+                    $('#drink').hide();
+                    });
+  });
